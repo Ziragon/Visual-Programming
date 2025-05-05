9f76e8be8c15b723a35584c40f64eb7c689a4a2a
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2025-05-05 19:07:28 +0700

    vs

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+835602dabf0349368b1601fb271aebfa48db5cfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7288648bd3a2d1a26f594cf51abc351b1fd6cefd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
